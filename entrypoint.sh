80f72752325f1a9d5ab2974f9052880eb43746f2
Mohammad Hossein <127041415+devmaster2003@users.noreply.github.com>
2023-04-06 21:33:49 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Define UUID and masquerade path, please modify it yourself. (Note: The masquerading path starts with / symbol, in order to avoid unnecessary trouble, please do not use special symbols.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+UUID=${UUID:-'0034d059-f9b3-412c-b931-4d585ba2a65a'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/support'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
 SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
